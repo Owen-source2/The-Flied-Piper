a40191c7391249a8314db6041986d723878efbde
Owen-source2 <ooleszewski@gmail.com>
2026-04-20 16:15:00 -0400

    Pharoh dialog fully implemented

--- Assets/Scripts/PharohDialogue.cs
+++ Assets/Scripts/PharohDialogue.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
-
+using TMPro;
+using System.Collections;
 public class PharohDialogue : MonoBehaviour
 {
     public bool isMad,isHappy;
-
-    public string[] happySayings;
+    public TMP_Text dialogBox;
+    public string[] happyLines;
     public string[] madLines;
+    public float textPersist,textCooldown;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void SelectLine()
     {
-        
+        if (isMad)
+        {
+            dialogBox.text=madLines[Random.Range(0,madLines.Length)];
+        }
+        else
+        {
+            dialogBox.text=happyLines[Random.Range(0,happyLines.Length)];
+        }
+        ClearLine();
     }    
+    void Start()
+    {
+        InvokeRepeating(nameof(SelectLine),textCooldown,textCooldown);
+    }
     void Update()
     {
         //Determine pharoh's mood for line selection
@@ -25,4 +40,10 @@ public class PharohDialogue : MonoBehaviour
             isMad=false;
         }
     }
+    IEnumerator ClearLine()
+    {
+        yield return new WaitForSeconds(textPersist);
+        dialogBox.text = null;
+    }
+
 }
