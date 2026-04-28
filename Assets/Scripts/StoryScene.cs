78c7cd137fcce8ecedeeed460f97125e9b8dac09
dfinch22 <david.finchh22@gmail.com>
2026-04-28 18:15:20 -0400

    Story Scene

--- Assets/Scripts/StoryScene.cs
+++ Assets/Scripts/StoryScene.cs
@@ -1,28 +1,14 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class StoryScene : MonoBehaviour
 {
     public AudioSource buttonSound;
 
     public void Play()
     {
         PlayClick();
-        SceneManager.LoadScene(1);
-    }
-
-    public void Options()
-    {
-        PlayClick();
-        Debug.Log("Options opened");
         SceneManager.LoadScene(2);
-    }
-
-    public void Quit()
-    {
-        PlayClick();
-        Debug.Log("Quit");
-        Application.Quit();
     }
 
     private void PlayClick()
